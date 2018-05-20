a081fd4cff4ab50f69244d3e373d5e6bcb0db302
wh <vildenhh@gmail.com>
2018-05-20 21:14:51 +0800

    bio client

---
+++ src/main/java/chapter1/bio/TimeClient.java
@@ -0,0 +1,31 @@
+package chapter1.bio;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class TimeClient {
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        Socket socket = null;
+        BufferedReader in = null;
+        PrintWriter out = null;
+        try {
+            socket = new Socket("127.0.0.1", port);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            out.println("QUERY TIME ORDER");
+            System.out.println("Send order 2 server succeed.");
+            String resp = in.readLine();
+            System.out.println("Now time is :" + resp);
+        } finally {
+            IOUtils.closeQuietly(socket);
+            IOUtils.closeQuietly(in);
+            IOUtils.closeQuietly(out);
+        }
+    }
+}
